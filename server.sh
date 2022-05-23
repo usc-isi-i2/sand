97107e7743691be0fa76eb37b5bf87a412d529bf
Binh Vu <binh@toan2.com>
2022-05-23 15:43:05 -0700

    update bash script to optionally start with different port

--- server.sh
+++ server.sh
@@ -15,11 +15,18 @@ else
     DIR=$2
 fi
 
+if [ "$3" == "" ]; then
+    PORT=5524
+else
+    PORT=$3
+fi
+
 python -m sand init --db $DIR/data/home/databases/sand.db
 
 python -m sand start $FLAG \
     --db $DIR/data/home/databases/sand.db \
-    --externaldb $DIR/data/home/databases
+    --externaldb $DIR/data/home/databases \
+    -p $PORT
 
 # python -m sand init --db $DIR/data/home/sand-demo/sand.db
 
