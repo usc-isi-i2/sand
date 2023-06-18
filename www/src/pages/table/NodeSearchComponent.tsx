f2e26fbab7b674476dc3f565fdab8041ad603cc0
P PUNITH KRISHNA <32421081+punith300i@users.noreply.github.com>
2023-06-17 19:59:32 -0700

    Revert "Added custom label and removed filterOptions for NodeComponentSearch"
    This reverts commit 4fb794bc0c71e7bf24cb2fa3a3a20f1020921742.

--- www/src/pages/table/NodeSearchComponent.tsx
+++ www/src/pages/table/NodeSearchComponent.tsx
@@ -45,7 +45,7 @@ export interface SearchResult {
 export interface SearchOptions {
   id: string;
   value: string;
-  label: any;
+  label: string;
   element: SearchResult;
   type: SMNodeType | "class";
 }
@@ -68,7 +68,9 @@ export const NodeSearchComponent = withStyles(styles)(
     } & WithStyles<typeof styles>) => {
       const { classStore } = useStores();
       const [searchOptions, setSearchOptions] = useState<SearchOptions[]>();
-      const [searchLoader, setSearchLoader] = useState<boolean>(false);
+      const [searchAPIOptions, setSearchAPIOptions] = useState<
+        SearchResult[]
+      >();
 
       // gather all options already in the store, leverage the fact
       // that property store is readonly
@@ -95,7 +97,7 @@ export const NodeSearchComponent = withStyles(styles)(
           return;
         }
         const searchResults: SearchOptions[] = [];
-        setSearchLoader(true);
+
         classStore
           .fetchSearchResults(query)
           .then((data) => {
@@ -104,17 +106,7 @@ export const NodeSearchComponent = withStyles(styles)(
                 type: "class",
                 id: searchResult.id,
                 element: searchResult,
-                // label: `${searchResult.label} (${searchResult.id})`,
-                label: (
-                  <div>
-                    <p style={{ color: "blue" }}>
-                      {searchResult.label} ({searchResult.id})
-                    </p>
-                    <p style={{ fontSize: 12, marginTop: -5 }}>
-                      {searchResult.description}
-                    </p>
-                  </div>
-                ),
+                label: `${searchResult.label} (${searchResult.id})`,
                 value: `class:${searchResult.id}`,
               });
             });
@@ -125,7 +117,6 @@ export const NodeSearchComponent = withStyles(styles)(
           })
           .then((searchResults) => {
             setSearchOptions([...searchResults, ...options]);
-            setSearchLoader(false);
             console.log(searchOptions);
           })
           .catch(function (error: any) {
@@ -138,23 +129,21 @@ export const NodeSearchComponent = withStyles(styles)(
           allowClear={true}
           options={searchOptions}
           onClear={() => setSearchOptions([...options])}
-          optionFilterProp="elemen.label"
+          optionFilterProp="label"
           className={classes.selection}
           showSearch={true}
-          // filterSort={(optionA, optionB) =>
-          //   (optionA?.element?.label ?? "")
-          //     .toLowerCase()
-          //     .localeCompare((optionB?.element?.label ?? "").toLowerCase())
-          // }
+          filterSort={(optionA, optionB) =>
+            (optionA?.label ?? "")
+              .toLowerCase()
+              .localeCompare((optionB?.label ?? "").toLowerCase())
+          }
           onSearch={debounce(onSearch, 300)}
-          filterOption={false}
           value={value === undefined ? undefined : `${value.type}:${value.id}`}
           onSelect={(value: any, option: SearchOptions) => {
             classStore.fetchById(option.id).then(() => {
               onSelect({ type: option.type, id: option.id });
             });
           }}
-          loading={searchLoader}
           onDeselect={(value: any, option: SearchValue) => {
             onDeselect({ type: option.type, id: option.id });
           }}
