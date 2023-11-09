bdde902ce2c61334679461ad0d6b7d878351fc37
Binh Vu <binh@toan2.com>
2023-11-09 11:48:02 -0800

    update kgdata

--- sand/extensions/wikidata.py
+++ sand/extensions/wikidata.py
@@ -1,15 +1,11 @@
 from dataclasses import dataclass
-from itertools import chain
 from typing import Mapping
 
-from dependency_injector.wiring import Provide, inject
 from hugedict.misc import identity
 from kgdata.wikidata import db
 from kgdata.wikidata.models import WDClass, WDEntity, WDProperty, WDValue
 from sm.namespaces.wikidata import WikidataNamespace
 
-from sand.config import AppConfig
-from sand.helpers.dependency_injection import autoinject
 from sand.models.base import StoreWrapper
 from sand.models.entity import Entity, Statement, Value
 from sand.models.ontology import OntClass, OntProperty, OntPropertyDataType
